824940edcd47b7bf390a228421f5215486b91fbd
keagan77 <keagansilveira77@gmail.com>
2025-01-19 13:22:18 +0530

    Image Handling Added

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -8,10 +8,33 @@ import { useNavigate } from 'react-router-dom';
 const CreateBlog = () => {
 
   const [html, setHtml] = useState('');
+  const [imageId, setImageId] = useState('');
   const navigate = useNavigate();
 
   function onChange(e) {
     setHtml(e.target.value);
+  }
+
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image",file);
+
+    const res = await fetch("http://localhost:8000/api/save-temp-image/",{
+      method: "POST",
+      body: formData
+    });
+
+    const result = await res.json();
+
+    //console.log(result);
+
+    if(result.status == false){
+      alert(result.errors.image);
+      e.target.value = null;
+    }
+
+    setImageId(result.image.id)
   }
   
   const {
@@ -22,7 +45,7 @@ const CreateBlog = () => {
   } = useForm()
 
   const formSubmit = async(data) => {
-      const newData = {...data,"description":html}
+      const newData = {...data,"description":html, image_id:imageId}
 
       const res = await fetch("http://localhost:8000/api/blogs", {
         method: "POST",
@@ -76,7 +99,7 @@ const CreateBlog = () => {
 
             <div className="mb-3">
               <label className='form-label'>Image</label> <br/>
-              <input type="file" />
+              <input onChange={handleFileChange} type="file" />
             </div>
 
             <div className="mb-3">
